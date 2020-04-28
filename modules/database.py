21abdd87c59b0986a3dcfeb0a9740d401f97bcf8
blipp <bclipp770@gmail.com>
2020-04-28 07:02:50 -0400

    basic connection

--- modules/database.py
+++ modules/database.py
@@ -19,15 +19,53 @@ class DatabaseManager():
         self.cursor = None
 
     def connect_db(self):
-        self.pool = psycopg2.pool.SimpleConnectionPool(1,
-                                                       20,
-                                                       user=self.config_dict["username"],
-                                                       password=self.config_dict["password"],
-                                                       host=self.config_dict["ip"],
-                                                       port=self.config_dict["port"],
-                                                       database=self.config_dict["database"])
+        user = self.config_dict["username"]
+        password = self.config_dict["password"]
+        host = self.config_dict["ip"]
+        # port = self.config_dict["port"]
+        database = self.config_dict["database"]
+        conn = psycopg2 \
+            .connect(f"dbname={database} user={user} host={host} password={password}")
+        self.cursor = conn.cursor()
+        self.conn = conn
+
+    def receive_sql_fetchall(self,
+                             sql: str) -> pd.DataFrame:
+        self.cursor.execute(sql)
+        return self.cursor.fetchall()
+
+    def send_sql(self,
+                 sql: str) -> pd.DataFrame:
+        self.execute(sql)
+
+    def close_conn(self):
+        self.cursor.close()
+        self.pool.putconn(self.conn)
+        self.pool.closeall
+
+
+class DatabaseManager2():
+    """
+
+    """
+
+    def __init__(self, config_dict):
+        self.config_dict = config_dict
+        self.conn = None
+        self.pool = None
+        self.cursor = None
+
+    def connect_db(self):
+        psypool = psycopg2.pool.SimpleConnectionPool(1,
+                                                     20,
+                                                     user=self.config_dict["username"],
+                                                     password=self.config_dict["password"],
+                                                     host=self.config_dict["ip"],
+                                                     port=self.config_dict["port"],
+                                                     database=self.config_dict["database"])
         if (self.pool):
             print("Connection pool created successfully")
+        self.pool = psypool
         self.conn = self.pool.getconn()
         self.cursor = self.conn.cursor()
 
@@ -55,5 +93,4 @@ def initialize_database(database_manager: DatabaseManager) -> pd.DataFrame:
                  "Dividends": 1,
                  "Stock Splits": 1}
     data_frame: pd.DataFrame = pd.DataFrame(test_dict, index=test_dict.keys())
-
     return data_frame
--- tests/test_intergration_tests.py
+++ tests/test_intergration_tests.py
@@ -3,7 +3,7 @@ intergration tests
 """
 
 from datetime import datetime
-from datetime import date, timedelta
+from datetime import timedelta
 
 import pandas as pd
 
@@ -23,19 +23,19 @@ def test_get_stock_data():
 def test_initialize_database():
     """
     """
-    test_dict = {"Open":1.2,
-                 "High":1.2,
-                 "Low":1.2,
-                 "Close":1.2,
-                 "Volume":1,
-                 "Dividends":1,
-                 "Stock Splits":1}
-    want:pd.Series = pd.DataFrame(test_dict,index=test_dict.keys())
-    config_dict:dict = {"ip":"127.0.0.1",
-                        "password":"test1234",
-                        "username":"test1234",
-                        "port": "5432",
-                        "database":"test1234"}
-    database_manager:database.DatabaseManager = database.DatabaseManager(config_dict)
+    test_dict = {"Open": 1.2,
+                 "High": 1.2,
+                 "Low": 1.2,
+                 "Close": 1.2,
+                 "Volume": 1,
+                 "Dividends": 1,
+                 "Stock Splits": 1}
+    want: pd.Series = pd.DataFrame(test_dict, index=test_dict.keys())
+    config_dict: dict = {"ip": "127.0.0.1",
+                         "password": "test1234",
+                         "username": "test1234",
+                         "port": "5432",
+                         "database": "test1234"}
+    database_manager: database.DatabaseManager = database.DatabaseManager(config_dict)
     dataframe: pd.DataFrame = database.initialize_database(database_manager)
     assert dataframe.dtypes.equals(want.dtypes)
