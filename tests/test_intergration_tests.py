df3d08f4a07e59dbd776fb1027804977d2a0e35f
blipp <bclipp770@gmail.com>
2020-04-28 06:38:53 -0400

    adding database interaction

--- tests/test_intergration_tests.py
+++ tests/test_intergration_tests.py
@@ -6,7 +6,9 @@ from datetime import datetime
 from datetime import date, timedelta
 
 import pandas as pd
+
 import modules.stock as stock
+import modules.database as database
 
 
 def test_get_stock_data():
@@ -16,3 +18,24 @@ def test_get_stock_data():
     start_date: str = (datetime.today() - timedelta(days=7)).strftime('%Y-%m-%d')
     stock_data_frame: pd.DataFrame = stock.get_stock(stock.stock_man, "TSLA", start_date)
     assert not stock_data_frame.empty
+
+
+def test_initialize_database():
+    """
+    """
+    test_dict = {"Open":1.2,
+                 "High":1.2,
+                 "Low":1.2,
+                 "Close":1.2,
+                 "Volume":1,
+                 "Dividends":1,
+                 "Stock Splits":1}
+    want:pd.Series = pd.DataFrame(test_dict,index=test_dict.keys())
+    config_dict:dict = {"ip":"127.0.0.1",
+                        "password":"test1234",
+                        "username":"test1234",
+                        "port": "5432",
+                        "database":"test1234"}
+    database_manager:database.DatabaseManager = database.DatabaseManager(config_dict)
+    dataframe: pd.DataFrame = database.initialize_database(database_manager)
+    assert dataframe.dtypes.equals(want.dtypes)
