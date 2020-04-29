b2a206b2482e01e377e718ff65aa297d3de21803
blipp <bclipp770@gmail.com>
2020-04-29 06:58:15 -0400

    working ml

--- modules/ml.py
+++ modules/ml.py
@@ -1,8 +1,55 @@
 from sklearn.preprocessing import StandardScaler
+from sklearn.pipeline import Pipeline
+from sklearn.compose import ColumnTransformer
+from sklearn.linear_model import ElasticNet
+from sklearn.model_selection import GridSearchCV
+
+
+import pandas as pd
 
 class MLManager():
 
+    def __init__(self, train_data_frame):
+        self.train_data_frame = train_data_frame
+        self.X = None
+        self.y = None
+        self.preprocessor = None
+        self.param_grid = None
 
-def get_training_data(table: str,
-                      symbol: str):
+    def create_xy(self):
+        self.X = self.train_data_frame.drop(['clse'],axis=1)
+        self.y = self.train_data_frame["clse"]
+
+    def preprocess_data(self):
+        numeric_transformer = Pipeline(steps=[
+            ('scaler', StandardScaler())])
+        numeric_features = numeric_features = self.X.select_dtypes(include=['int64', 'float64']).columns
+        self.preprocessor = ColumnTransformer(
+            transformers=[
+                ('num', numeric_transformer, numeric_features)])
+        self.en = Pipeline(steps=[('preprocessor', self.preprocessor),
+                             ('regressor', ElasticNet())])
+
+    def grid_search(self):
+        self.param_grid = {
+            'alpha': [200, 500],
+            'l1_ratio': [],
+            'classifier__max_depth': [4, 5, 6, 7, 8],
+            'classifier__criterion': ['gini', 'entropy']}
+        self.CV = GridSearchCV(self.en, self.param_grid, n_jobs=1)
+
+    def train_cv(self):
+        self.CV.fit(self.X, self.Y)
+        print(self.CV.best_params_)
+        print(self.CV.best_score_)
+
+    def predict(self):
+
+
+
+
+def get_training_data(train_data_frame: pd.DataFrame,
+                      ml_manager:MLManager,
+                      preposessing=True):
+
     #
--- tests/test_ml.py
+++ tests/test_ml.py
@@ -11,13 +11,22 @@ def test_get_training_data():
     """
     Used to test the basic functionality of get_stock with a start date
     """
+    test_dict = {"Open": 1.2,
+                 "High": 1.2,
+                 "Low": 1.2,
+                 "Close": 1.2,
+                 "Volume": 1,
+                 "Dividends": 1,
+                 "Stock Splits": 1}
+    train_data_frame: pd.DataFrame = pd.DataFrame(test_dict, index=test_dict.keys())
     ml_manager: Mock()
-    data_frame: pd.DataFrame = ml.get_training_data(ml_manager,
-                                                    preposessing=True)
-    assert not data_frame.empty
+    got: pd.DataFrame = ml.get_training_data(train_data_frame,
+                                                    ml_manager,
+                                                    True)
+    assert not got.empty
 
 def test_grid_search():
 
 def test_predict():
-    
+
 def test_save_model():
