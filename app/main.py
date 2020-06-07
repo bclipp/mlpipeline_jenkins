1939fd3499c1873e3846a2ece40b3de4513078ca
blipp <bclipp770@gmail.com>
2020-06-07 12:48:43 -0400

    adding comments

--- app/main.py
+++ app/main.py
@@ -8,6 +8,11 @@ import modules.utils as utils
 def main():
     """
     This function accepts cmd line args needed for choice of action.
+        - actions:
+            * init_db : setup tables needed for stocks
+            * upload_this_weeks_stock:  pull a weeks worth of stock data
+            * upload_ayear_stock: pull a year's work of stock data
+            * search_train_model: train a gmm model on the stock in the database
     """
     config: dict = {"db_ip": "postgres",
                     "mlflow_ip": "mlflow",
@@ -24,7 +29,7 @@ def main():
             utils.upload_this_weeks_stock(config, "TSLA")
         elif args.task == "upload_ayear_stock":
             utils.upload_ayear_stock(config, "TSLA")
-        elif args.task == "search_train_model":
+        elif args.task == "search_train_model_gmm":
             utils.search_train_gmm_model(config, "stocks")
     else:
         print("no valid arg where provided")
