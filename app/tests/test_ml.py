21de865fb86a2e83500ad98c3e8e91bcbfb0cfe3
blipp <bclipp770@gmail.com>
2020-06-08 21:04:45 -0400

    better bastracted model

--- app/tests/test_ml.py
+++ app/tests/test_ml.py
@@ -63,8 +63,10 @@ TEST_DATA = [
 
 
 @pytest.mark.parametrize("data_frame,wanted", TEST_DATA)
-def test_preprocess_data_2(data_frame, wanted):
+def test_preprocess_data(data_frame, wanted):
     """
+    test_preprocess_data is used for  testing that the preprocessing is done correctly.
+    Currently there is only label encoding.
 
     :param data_frame:
     :param wanted:
@@ -80,3 +82,41 @@ def test_preprocess_data_2(data_frame, wanted):
     print(got)
     print(f"sum {got} wanted {wanted}")
     assert got.equals(wanted)
+
+
+def test_grid_search_gmm():
+    """
+    test_grid_search_gmm is used to verify that the basic functionality of training
+    the GMM model works
+    
+    :param data_frame:
+    :param wanted:
+    :return:
+    """
+    data_frame = pd.DataFrame(
+        columns=["Open",
+                 "High",
+                 "Low",
+                 "clse",
+                 "Volume",
+                 "Dividends",
+                 "Stock Splits"],
+        data=[[1, 2, "b", 3, 4, 5, 3],
+              [1, 2, "b", 3, 4, 5, 3],
+              [1, 2, "b", 3, 4, 5, 3],
+              [1, 2, "b", 3, 4, 5, 3],
+              [1, 2, "b", 3, 4, 5, 3],
+              [1, 2, "b", 3, 4, 5, 3],
+              [1, 2, "b", 3, 4, 5, 3],
+              [1, 2, "b", 3, 4, 5, 3],
+              [1, 2, "b", 3, 4, 5, 3],
+              [1, 2, "b", 3, 4, 5, 3],
+              [1, 2, "b", 3, 4, 5, 3],
+              [1, 2, "b", 3, 4, 5, 3]])
+    mlflow = Mock()
+    config: dict = {"mlflow_ip": "mlflow"}
+    gmm_ml_manager: ml_gmm.GmmMlManager = ml_gmm.GmmMlManager(pd.DataFrame(data_frame),
+                                                              config,
+                                                              mlflow)
+    gmm_ml_manager.preprocess_data()
+    gmm_ml_manager.grid_search_gmm()
